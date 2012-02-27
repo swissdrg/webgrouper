e6e2f31ad0f2709bc6c637bad5a071b1d8da63a1
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-02-27 16:43:31 +0100

    Added skeleton

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+gem 'rails', '3.1.0'
+gem 'jruby-openssl'
+gem 'json'
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webgrouper::Application.config.secret_token = '77489079423c9d26aa583d48579129a47c8ec5ea2f90e89b90abd715ba4fd169a80aaf9e5692862780a32ec4a4fa4f58d70af14319972d0bd40bf73441923663'
