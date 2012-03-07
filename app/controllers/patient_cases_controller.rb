54a1c0501693b54e0a4e1d05df9d9bcf568d2b66
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-03-07 20:04:50 +0100

    Prepared some methods for wrapper matching

--- app/controllers/patient_cases_controller.rb
+++ app/controllers/patient_cases_controller.rb
@@ -1,2 +1,23 @@
 class PatientCasesController < ApplicationController
+  
+  def new
+    @webgrouper_patient_case = WebgrouperPatientCase.new
+  end
+  
+  def create
+    @webgrouper_patient_case = WebgrouperPatientCase.new(params[:webgrouper_patient_case])
+    if @webgrouper_patient_case.valid?
+      group(@webgrouper_patient_case)
+    else
+      render 'new'
+    end  
+  end
+  
+  def group(webgrouper_patient_case)
+    patient_case = webgrouper_patient_case.wrapper_patient_case
+    kernel = GroupKernel.create("spec.bin")
+    @result = kernel.group(patient_case)
+    
+  end
+  
 end
--- app/models/webgrouper_patient_case.rb
+++ app/models/webgrouper_patient_case.rb
@@ -4,11 +4,29 @@ class WebgrouperPatientCase
   include ActiveModel::Conversion
   extend ActiveModel::Naming
   
-  attr_accessor :entry_date, :exit_date, :birth_date, :leave_days, :age_years, :age_days, :adm_weight, :sex, :pdx, :diagnoses, :procedures
+  attr_accessor :entry_date, 
+                :exit_date, 
+                :birth_date, 
+                :leave_days, 
+                :age_years, 
+                :age_days, 
+                :adm_weight, 
+                :sex, 
+                :adm, 
+                :sep,
+                :los,
+                :sdf,
+                :hmv, 
+                :pdx, 
+                :diagnoses, 
+                :procedures
   
   validates :age_years, :presence => true
+  validates :sex,       :presence => true
   
   def initialize(attributes = {})
+    diagnoses = []
+    procedures = []
     attributes.each do |name, value|
       send("#{name}=", value)
     end
@@ -16,6 +34,28 @@ class WebgrouperPatientCase
   
   def persisted?
     false
+  end
+  
+  def wrapper_patient_case
+    pc = PatientCase.new
+    pc.entry_date = entry_date unless entry_date.nil?
+    pc.exit_date = exit_date unless exit_date.nil?
+    pc.birth_date = birth_date unless birth_date.nil?
+    pc.leave_days = leave_days unless leave_days.nil?
+    pc.age_years = age_years unless age_years.nil?
+    pc.age_days = age_days unless age_days.nil?
+    pc.adm_weight = adm_weight unless adm_weight.nil?
+    pc.sex = sex unless sex.nil?
+    pc.adm = adm unless adm.nil?
+    pc.sep = sep unless sep.nil?
+    pc.los = los unless los.nil?
+    pc.sdf = sdf unless sdf.nil?
+    pc.hmv = hmv unless hmv.nil?
+    pc.pdx = pdx unless pdx.nil?
+    pc.diagnoses = diagnoses
+    pc.procedures = procedures
+    
+    pc
   end
   
 end
