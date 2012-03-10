cf676a7a0ba784a35d2c7d2e2a5212ec3fb6fb78
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-03-10 14:49:22 +0100

    Refactored patient case model

--- app/controllers/patient_cases_controller.rb
+++ app/controllers/patient_cases_controller.rb
@@ -5,19 +5,16 @@ class PatientCasesController < ApplicationController
   end
   
   def create
-    attributes = params[:webgrouper_patient_case]
-    @webgrouper_patient_case = WebgrouperPatientCase.new(attributes)
+    @webgrouper_patient_case = WebgrouperPatientCase.new(params[:webgrouper_patient_case])
     if @webgrouper_patient_case.valid?
-      group(attributes)
+      group(@webgrouper_patient_case)
     else
-      @webgrouper_patient_case = WebgrouperPatientCase.new
       flash.now[:error] = "Die Validierung ist fehlgeschlagen."
       render 'index'
     end  
   end
   
-  def group(attributes)
-    patient_case = WebgrouperPatientCase.wrapper_patient_case_for(attributes)
+  def group(patient_case)
     kernel = org.swissdrg.grouper.kernel.GrouperKernel.create("spec.bin")
     @result = kernel.group(patient_case)
     render 'index'
--- app/models/webgrouper_patient_case.rb
+++ app/models/webgrouper_patient_case.rb
@@ -1,58 +1,33 @@
-class WebgrouperPatientCase
+class WebgrouperPatientCase < PatientCase
   
   include ActiveModel::Validations
   include ActiveModel::Conversion
   extend ActiveModel::Naming
-  
-  attr_accessor :entry_date, 
-                :exit_date,
-                :birth_date, 
-                :leave_days, 
-                :age_years, 
-                :age_days, 
-                :adm_weight, 
-                :sex, 
-                :adm, 
-                :sep,
-                :los,
-                :sdf,
-                :hmv, 
-                :pdx, 
-                :diagnoses,
-                :procedures
   
   #TODO: Add more validations
   validates :age_years, :presence => true
   validates :sex,       :presence => true
   
   def initialize(attributes = {})
-    attributes.each do |name, value|
+    super()
+    prepare_for_grouping(attributes = {}) do |name, value|
       send("#{name}=", value)
     end
   end
   
-  def persisted?
-    false
-  end
-  
-  def self.wrapper_patient_case_for(attributes = {})
-    patient_case = PatientCase.new
-    prepare_for_grouping(patient_case, attributes) do |name, value|
-      patient_case.send("set_#{name}", value) unless value.nil?
-    end
-    patient_case
-  end
-  
-  
   private
-    
-    def self.prepare_for_grouping(patient_case, attributes = {}, &block)
+  
+    def prepare_for_grouping(attributes = {}, &block)
       attributes.each do |name, value|
-        if patient_case.send(name).is_a? Fixnum
+        if super.send(name).is_a? Fixnum
           value = value.to_i 
         end
         yield name, value
       end
     end
+  
+  def persisted?
+    false
+  end
   
 end
