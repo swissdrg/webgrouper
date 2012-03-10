a32b5b322f029706abcaf5064485a64f33f9a50d
Michael Single <silent.simplay@gmail.com>
2012-03-10 15:16:23 +0100

    documentation for class WebgrouperPatientCase

--- app/models/webgrouper_patient_case.rb
+++ app/models/webgrouper_patient_case.rb
@@ -1,3 +1,7 @@
+# WebgrouperPatientCase holds all input variables for a certain patiant case
+# you can find in either the entry date, the exit date and the number of leave days. 
+# WebgrouperPatientCase inherits from the java class PatientCase
+# autors team1
 class WebgrouperPatientCase < PatientCase
   
   include ActiveModel::Validations
@@ -8,6 +12,8 @@ class WebgrouperPatientCase < PatientCase
   validates :age_years, :presence => true
   validates :sex,       :presence => true
   
+  # invokes superconstructor of java class PatientCase
+	# prepares values of attribute hash for the ruby patient class.
   def initialize(attributes = {})
     super()
     prepare_for_grouping(attributes = {}) do |name, value|
@@ -16,7 +22,8 @@ class WebgrouperPatientCase < PatientCase
   end
   
   private
-  
+  	
+	  # cleans up attribute values and if necessary corrects data type
     def prepare_for_grouping(attributes = {}, &block)
       attributes.each do |name, value|
         if super.send(name).is_a? Fixnum
