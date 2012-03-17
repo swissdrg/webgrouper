b1ff8da3c238824416e74247031fbde618650c56
panmari <smmuzi@gmail.com>
2012-03-17 14:58:07 +0100

    Refactored JS

--- app/assets/javascripts/patient_cases.js
+++ app/assets/javascripts/patient_cases.js
@@ -1,16 +1,37 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
-	//should find everything einding in "date"
+	addDatePickers()
+	admWeightControl(0);
+	disableTabForLinks();
+});
+
+
+/**
+ * Kinda funny, but doesn't work for all browsers
+ */
+function disableTabForLinks() {
+	$('a').each(function() {
+	    this.setAttribute( 'tabindex', '-1' );
+	});
+}
+/**
+ * Adds date pickers to every input field of the class "date_picker"
+ * The format of the date picker is eg 02.04.2011
+ */
+function addDatePickers() {
 	$(".date_picker").each(function() {
 		var form_id = this.id;
 		var eles = {};
 		eles[form_id] = "d-dt-m-dt-Y";
 		datePickerController.createDatePicker({formElements: eles});
 	});
-	admWeightControl(0);
-});
-
+}
+/**
+ * Lets the id "admWeight" dissapear according to the
+ * settings in the field age_mode
+ * @param fade_time the time used to fade it in/out
+ */
 function admWeightControl(fade_time) {
 	if ($('[id$="age_mode"]').val() == "days") {
 		$("#admWeight").show(fade_time);
