2787724b5f20b75d515bad9fac8d04e03dee8d1a
panmari <smmuzi@gmail.com>
2012-03-26 15:14:28 +0200

    Added some steps

---
+++ features/step_definitions/fill_in.rb
@@ -0,0 +1,19 @@
+Given /^the form with initialized standard values$/ do
+  visit path_to webgrouper_patient_case
+end
+
+When /^I enter some random \(valid!\) data$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I press on "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the grouping should succeed$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the result should be shown$/ do
+  pending # express the regexp above with the code you wish you had
+end
