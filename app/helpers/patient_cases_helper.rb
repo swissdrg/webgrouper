8a5f4ec3075ddb65b62aa749720abc9820e57da3
Simon Kiener <jugglinghobo@gmail.com>
2012-03-30 21:57:38 +0200

    added helper to seperate compressed procedure strings.

--- app/helpers/patient_cases_helper.rb
+++ app/helpers/patient_cases_helper.rb
@@ -1,2 +1,10 @@
 module PatientCasesHelper
+  
+  # def procedure_values(field_counter)
+  #   puts "THERESE! #{patient_case}"
+  #   compressed_procedure = procedures[field_counter] ## RADI FIX
+  #   procedures = compressed_procedure.split("$")
+  #   puts "#{procedures}"
+  #   @procedures = procedures
+  # end
 end
