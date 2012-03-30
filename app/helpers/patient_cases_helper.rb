709e6c351f43cf14ada8529e030448acbca320dd
Simon Kiener <jugglinghobo@gmail.com>
2012-03-30 22:56:26 +0200

    procedures as values in form after page reload.

--- app/helpers/patient_cases_helper.rb
+++ app/helpers/patient_cases_helper.rb
@@ -1,10 +1,8 @@
 module PatientCasesHelper
   
   def procedure_values(patient_case, field_counter)
-      puts "THERESE!"
-      compressed_procedure = procedures[field_counter]
+      compressed_procedure = patient_case.procedures[field_counter]
       procedures = compressed_procedure.split("$")
-      puts "#{procedures}"
       procedures
     end
 end
