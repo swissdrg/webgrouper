be4db40c9f6008d7fcba5464114ad021cb5348b9
Simon Kiener <jugglinghobo@gmail.com>
2012-03-30 23:12:06 +0200

    hotfix for rathrio

--- app/helpers/patient_cases_helper.rb
+++ app/helpers/patient_cases_helper.rb
@@ -1,8 +1,8 @@
 module PatientCasesHelper
   
-  def procedure_values(patient_case, field_counter)
-      compressed_procedure = patient_case.procedures[field_counter]
-      procedures = compressed_procedure.split("$")
-      procedures
-    end
+  # def procedure_values(patient_case, field_counter)
+  #     compressed_procedure = patient_case.procedures[field_counter]
+  #     procedures = compressed_procedure.split("$")
+  #     procedures
+  #   end
 end
