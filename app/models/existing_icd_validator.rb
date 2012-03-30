f95b7d6828c92efc83262e3ded540ab8c3266465
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-03-30 22:03:16 +0200

    Added some doc to validate method

--- app/models/existing_icd_validator.rb
+++ app/models/existing_icd_validator.rb
@@ -1,4 +1,5 @@
 class ExistingIcdValidator < ActiveModel::EachValidator
+  #will automatically be called if one should validate existing_icd as true
   def validate_each(record, attribute, value)
     short_code = value.gsub(/\./, "").strip
     record.errors[attribute] << "invalid" if ICD.find_by_IcShort(short_code).nil?
