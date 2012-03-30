ef231ffbcf8bc170d49b7ce98bdd8155889a15fd
Simon Kiener <jugglinghobo@gmail.com>
2012-03-30 22:36:23 +0200

    another try

--- app/helpers/patient_cases_helper.rb
+++ app/helpers/patient_cases_helper.rb
@@ -1,10 +1,10 @@
 module PatientCasesHelper
   
-  # def procedure_values(field_counter)
-  #     puts "THERESE!"
-  #     compressed_procedure = procedures[field_counter]
-  #     procedures = compressed_procedure.split("$")
-  #     puts "#{procedures}"
-  #     procedures
-  #   end
+  def procedure_values(patient_case, field_counter)
+      puts "THERESE!"
+      compressed_procedure = procedures[field_counter]
+      procedures = compressed_procedure.split("$")
+      puts "#{procedures}"
+      procedures
+    end
 end
