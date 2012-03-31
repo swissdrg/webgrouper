681a1802946f5afd37837364dd2d640612a82bbf
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-03-31 17:00:28 +0200

    Minor fix. And added first spec

--- app/controllers/webgrouper_patient_cases_controller.rb
+++ app/controllers/webgrouper_patient_cases_controller.rb
@@ -23,7 +23,7 @@ class WebgrouperPatientCasesController < ApplicationController
 		weighting_relation.setDrg(@result.getDrg)
 		weighting_relation.setCostWeight(drg.cost_weight)
 		weighting_relation.setAvgDuration(drg.avg_duration)
-		weighting_relation.setFirstDayDiscount(drg.set_first_day_discount)
+		weighting_relation.setFirstDayDiscount(drg.first_day_discount)
 		weighting_relation.setFirstDaySurcharge(drg.first_day_surcharge)
 		weighting_relation.setSurchargePerDay(drg.surcharge_per_day)
 		weighting_relation.setDiscountPerDay(drg.discount_per_day)
---
+++ spec/models/webgrouper_patient_case_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe WebgrouperPatientCase do
+  
+  describe "initializer" do
+    
+    it "should have an initialize method" do
+      WebgrouperPatientCase.should respond_to :new
+    end
+    
+  end
+  
+end
