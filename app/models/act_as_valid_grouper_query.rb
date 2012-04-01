11ced0dd9ce4505fe493e13e7a30b8fd3cda5e5d
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-04-01 17:03:18 +0200

    Added back date validations

--- app/models/act_as_valid_grouper_query.rb
+++ app/models/act_as_valid_grouper_query.rb
@@ -7,9 +7,9 @@ module ActAsValidGrouperQuery
     base.send :include, ActiveModel::Conversion
     
     base.validates      :sex,             :presence => true
-    # base.validates_date :entry_date,      :on_or_before => :today, :allow_blank => true
-    # base.validates_date :exit_date,       :on_or_before => :today, :after => :entry_date, :allow_blank => true
-    # base.validates_date :birth_date,      :on_or_before => :today, :allow_blank => true
+    base.validates_date :entry_date,      :on_or_before => :today, :allow_blank => true
+    base.validates_date :exit_date,       :on_or_before => :today, :after => :entry_date, :allow_blank => true
+    base.validates_date :birth_date,      :on_or_before => :today, :allow_blank => true
     base.validates      :leave_days,      :numericality => { :only_integer => true, :greater_than_or_equal_to => 0}
     base.validates      :age,             :presence => true, :numericality => { :only_integer => true, :greater_than => 0, :less_than => 125}, :unless => :age_mode_days?
     base.validates      :age,             :presence => true, :numericality => { :only_integer => true, :greater_than => 0, :less_than => 367}, :if => :age_mode_days?
