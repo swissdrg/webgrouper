70cced0a544c48eb4587a112861a29a442e675b4
Simon Kiener <jugglinghobo@gmail.com>
2012-04-12 21:06:21 +0200

    css a bit better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
       
   def link_to_add_fields(name, kind)  
-    link_to_function(name, "add_fields(\"#{kind}\", \"#{escape_javascript(row(kind))}\", \"\")")
+    link_to_function(image_tag(name), "add_fields(\"#{kind}\", \"#{escape_javascript(row(kind))}\", \"\")")
   end
   
   def link_to_remove_fields(name, kind)
-    link_to_function(name, "remove_fields(\"#{kind}\")")
+    link_to_function(image_tag(name), "remove_fields(\"#{kind}\")")
   end
     
   def row(kind)
