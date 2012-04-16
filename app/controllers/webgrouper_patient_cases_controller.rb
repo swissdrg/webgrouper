08a05ca94c2f43893a200695ec619069dc5b7127
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-04-16 17:32:49 +0200

    Minor shotgun tweaks to group method

--- app/controllers/webgrouper_patient_cases_controller.rb
+++ app/controllers/webgrouper_patient_cases_controller.rb
@@ -16,24 +16,25 @@ class WebgrouperPatientCasesController < ApplicationController
   
   def group(patient_case) 
 		@result = GROUPER.group(patient_case)
-		weighting_relation = WeightingRelation.new		
-		drg = DRG.find_by_DrCode(@result.getDrg)
+		@weighting_relation = WeightingRelation.new
+		all_drg = DRG.where(:DrFKSyID => 9)
+		drg = all_drg.find_by_DrCode(@result.getDrg)
     		
-		weighting_relation.setDrg(@result.getDrg)
+		@weighting_relation.setDrg(@result.getDrg)
 		
 		@factor = 10000
 		
-		weighting_relation.setCostWeight(drg.cost_weight*@factor)
-		weighting_relation.setAvgDuration(drg.avg_duration*@factor)
-		weighting_relation.setFirstDayDiscount(drg.first_day_discount)
-		weighting_relation.setFirstDaySurcharge(drg.first_day_surcharge)
-		weighting_relation.setSurchargePerDay(drg.surcharge_per_day*@factor)
-		weighting_relation.setDiscountPerDay(drg.discount_per_day*@factor)
-		weighting_relation.setTransferFlatrate(drg.transfer_flatrate*@factor)
-		weighting_relation.setUseTransferFlatrate(drg.transfer)
+		@weighting_relation.setCostWeight(drg.cost_weight*@factor)
+		@weighting_relation.setAvgDuration(drg.avg_duration*@factor)
+		@weighting_relation.setFirstDayDiscount(drg.first_day_discount)
+		@weighting_relation.setFirstDaySurcharge(drg.first_day_surcharge)
+		@weighting_relation.setSurchargePerDay(drg.surcharge_per_day*@factor)
+		@weighting_relation.setDiscountPerDay(drg.discount_per_day*@factor)
+		@weighting_relation.setTransferFlatrate(drg.transfer_flatrate*@factor)
+		@weighting_relation.setUseTransferFlatrate(drg.transfer)
 		
 		@base_cost_weight = drg.cost_weight
-		@cost_weight = GROUPER.calculateEffectiveCostWeight(patient_case, weighting_relation)
+		@cost_weight = GROUPER.calculateEffectiveCostWeight(patient_case, @weighting_relation)
     render 'index'
   end
   
