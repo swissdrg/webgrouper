065c36968ec75dd2dc8aca91821375ba8f985452
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-04-18 16:50:32 +0200

    Added helpers to set and get the current system

--- app/helpers/webgrouper_patient_cases_helper.rb
+++ app/helpers/webgrouper_patient_cases_helper.rb
@@ -1,8 +1,18 @@
 module WebgrouperPatientCasesHelper
   
+  # Is this still in use somewhere?
   def procedure_values(patient_case, field_counter)
     compressed_procedure = patient_case.procedures[field_counter]
     procedures = compressed_procedure.split("$") unless compressed_procedure.nil?
     procedures
   end
+  
+  def current_system=(system)
+    @current_system = system
+  end
+  
+  def current_system
+    @current_sytem ||= System.find_by_SyID(9)
+  end
+  
 end
