f933e20a197ae22f9356b064652c810ba07ebd35
Michael Single <silent.simplay@gmail.com>
2012-04-21 16:50:01 +0200

    extended webgrouper patient case helper (used in patient case controller to receive appropriate specification file path after switching 'kernel version' in the gui)

--- app/helpers/webgrouper_patient_cases_helper.rb
+++ app/helpers/webgrouper_patient_cases_helper.rb
@@ -1,17 +1,50 @@
 module WebgrouperPatientCasesHelper
   
   def spec_path(system_id)
+		is_64bit = false	
+		if java.lang.System.getProperty('os.arch').include?('64')
+			is_64bit = true		
+		end
+		
     path = case system_id
       when 9
         # path for billing 10 spech
+				if is_64bit
+					File.join('lib','specs','Spec10billing64bit.bin')
+				else File.join('lib','specs','Spec10billing32bit.bin')
+				end
       when 8
+				if is_64bit
+					File.join('lib','specs','Spec10planning2_64bit.bin')
+				else 
+					File.join('lib','specs','Spec10planning2.bin')
+				end
       when 7
+				if is_64bit
+					File.join('lib','specs','Spec10catalogue64bit.bin')
+				else 
+					File.join('lib','specs','Spec10catalogue.bin')
+				end
       when 6
+				if is_64bit
+					File.join('lib','specs','Spec03billing64bit.bin')
+				else 
+					File.join('lib','specs','Spec03billing64bit.bin')
+				end
       when 5
+				if is_64bit
+					File.join('lib','specs','Spec03planning64bit.bin')
+				else 
+					File.join('lib','specs','Spec03planning.bin')
+				end	
       when 4
+				# currently we dont have those spec files
       when 2
+				# currently we dont have those spec files
       when 1
+				# currently we dont have those spec files
     end
+		
   end
   
 end
