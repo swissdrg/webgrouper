16db30ec8b3b213b9d959dc1b051efc851163504
panmari <smmuzi@gmail.com>
2012-04-22 02:38:02 +0200

    Chart computes what we want, but not pretty enough!

--- app/models/los_data_table.rb
+++ app/models/los_data_table.rb
@@ -1,16 +1,29 @@
 class LosDataTable < GoogleVisualr::DataTable
-  def initialize()
+  def initialize(actual_los, weighting_relation, factor)
     super()
-    new_column('string', 'label')
-    new_column('number', 'Tage')
-    new_column('number', I18n.t('result.cost-weight.legend'))
+    new_columns [ {:type => 'number', :label => 'Tage'},
+                  {:type => 'number', :label => I18n.t('result.cost-weight.legend')} ]
+    #new_column :type => 'string', :label => 'Label'
+    @avg_duration = weighting_relation.avg_duration.to_f/factor
+    @low_trim_point = weighting_relation.first_day_discount + 1
+    @high_trim_point = weighting_relation.first_day_surcharge - 1
+    @base_cost_rate = weighting_relation.cost_weight.to_f/factor
+    discount_per_day = weighting_relation.discount_per_day.to_f/factor
+    surcharge_per_day = weighting_relation.surcharge_per_day.to_f/factor
+    one_day_cost_rate = @base_cost_rate - discount_per_day*(@low_trim_point - 1)
+    many_days = [@high_trim_point, actual_los].max + 5
+    many_days_cost_rate = @base_cost_rate + surcharge_per_day*(many_days - @high_trim_point)
     add_rows([
-    ['blub',10,10],
-    ['asdf',15,15]]) 
+    [1, one_day_cost_rate],
+    [@low_trim_point, @base_cost_rate],
+    #[actual_los, ]
+    [@avg_duration, @base_cost_rate],
+    [@high_trim_point, @base_cost_rate],
+    [many_days, many_days_cost_rate]]) 
   end
   
   def make_chart()
-    options = {:width => 600, :height => 170, :title => 'Diagramm'}
+    options = {:width => 600, :height => 170, :hAxis => {:minValue => 0}}
     GoogleVisualr::Interactive::AreaChart.new(self, options)
   end
 end
