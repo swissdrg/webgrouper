f54bb3a78c27c237cdb725dfe38eafef52fa4607
panmari <smmuzi@gmail.com>
2012-04-22 03:25:53 +0200

    added annotations for graphic

--- app/models/los_data_table.rb
+++ app/models/los_data_table.rb
@@ -1,29 +1,39 @@
+# This class is used for creating a data chart containing all the
+# length of stay data computed through the grouper.
+# This uses a big deal of the google chart API
 class LosDataTable < GoogleVisualr::DataTable
-  def initialize(actual_los, weighting_relation, factor)
+  def initialize(actual_los, effective_cost_weight, weighting_relation, factor)
     super()
     new_columns [ {:type => 'number', :label => 'Tage'},
                   {:type => 'number', :label => I18n.t('result.cost-weight.legend')} ]
+    new_column('string', nil, nil, 'annotation')
+    new_column('string', nil, nil, 'annotationText')
     #new_column :type => 'string', :label => 'Label'
     @avg_duration = weighting_relation.avg_duration.to_f/factor
     @low_trim_point = weighting_relation.first_day_discount + 1
     @high_trim_point = weighting_relation.first_day_surcharge - 1
     @base_cost_rate = weighting_relation.cost_weight.to_f/factor
+    @effective_cost_weight = effective_cost_weight.to_f/factor
     discount_per_day = weighting_relation.discount_per_day.to_f/factor
     surcharge_per_day = weighting_relation.surcharge_per_day.to_f/factor
     one_day_cost_rate = @base_cost_rate - discount_per_day*(@low_trim_point - 1)
     many_days = [@high_trim_point, actual_los].max + 5
     many_days_cost_rate = @base_cost_rate + surcharge_per_day*(many_days - @high_trim_point)
+    
     add_rows([
-    [1, one_day_cost_rate],
-    [@low_trim_point, @base_cost_rate],
-    #[actual_los, ]
-    [@avg_duration, @base_cost_rate],
-    [@high_trim_point, @base_cost_rate],
-    [many_days, many_days_cost_rate]]) 
+    [1, one_day_cost_rate, '',''],
+    [@low_trim_point, @base_cost_rate, 'lo', I18n.t('result.length-of-stay.low_trim_point')],
+    [@avg_duration, @base_cost_rate, 'mid',I18n.t('result.length-of-stay.average_los')],
+    [@high_trim_point, @base_cost_rate, 'hi', I18n.t('result.length-of-stay.high_trim_point')],
+    [actual_los, @effective_cost_weight, '***', I18n.t('result.length-of-stay.length-of-stay')],
+    [many_days, many_days_cost_rate, '','']]) 
   end
   
   def make_chart()
-    options = {:width => 600, :height => 170, :hAxis => {:minValue => 0}}
-    GoogleVisualr::Interactive::AreaChart.new(self, options)
+    options = { :width => 650, :height => 170,
+                :legend => 'none',
+                :hAxis => { :title => 'Tage' },
+                :vAxis => { :title => 'Kostengewicht' }}
+    GoogleVisualr::Interactive::LineChart.new(self, options)
   end
 end
