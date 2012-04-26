891a4741b735f9ede43aa0a8d621d34cc8159102
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-04-26 21:37:55 +0200

    Moved utility classes to lib/util folder

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Webgrouper
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{Rails.root}/app/models/validators)
     config.autoload_paths += %W(#{Rails.root}/app/models/core)
+    config.autoload_paths += %W(#{Rails.root}/lib/util)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
