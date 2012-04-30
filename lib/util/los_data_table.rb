f8d0f4c757d2bc7e1773060234bd436306ed9024
panmari <smmuzi@gmail.com>
2012-04-30 16:05:16 +0200

    added tooltip, some small fixes

--- lib/util/los_data_table.rb
+++ lib/util/los_data_table.rb
@@ -8,7 +8,7 @@ class LosDataTable < GoogleVisualr::DataTable
                   {:type => 'number', :label => I18n.t('result.cost-weight.legend')},
                   {:type => 'string', :role => 'annotation'},
                   {:type => 'string', :role => 'annotationText'},
-                  {:type => 'string', :role => 'tooltip'}, #TODO: make a pretty tooltip
+                  {:type => 'string', :role => 'tooltip'},
                   {:type => 'number', :label => I18n.t('transferred')} ]
 
     # Values used for populating the rows of the table:
@@ -30,17 +30,19 @@ class LosDataTable < GoogleVisualr::DataTable
       one_day_transfer_rate = base_cost_rate - transfer_flatrate*(avg_duration-1)
       avg_transfer_rate = base_cost_rate
       rows.push [actual_los, nil, '***', I18n.t('result.length-of-stay.length-of-stay'),  
-                  '', effective_cost_weight]
+                 make_tooltip('length-of-stay', actual_los, effective_cost_weight), effective_cost_weight]
     else
-      rows.push [actual_los, effective_cost_weight, '***', I18n.t('result.length-of-stay.length-of-stay'), nil , nil]
+      rows.push [actual_los, effective_cost_weight, '***', I18n.t('result.length-of-stay.length-of-stay'), 
+                 make_tooltip('length-of-stay', actual_los, effective_cost_weight) , nil]
     end
     
     rows.push [1, one_day_cost_rate, '','','', one_day_transfer_rate],
-    [low_trim_point, base_cost_rate, 'lo', I18n.t('result.length-of-stay.low_trim_point'), 
-    I18n.t('result.length-of-stay.low_trim_point') + ": " + low_trim_point.to_s +
-    '\n'  + I18n.t('result.cost-weight.legend') + ': '+ base_cost_rate.to_s, nil],
-    [avg_duration, base_cost_rate, 'avg',I18n.t('result.length-of-stay.average_los'),nil, avg_transfer_rate],
-    [high_trim_point, base_cost_rate, 'hi', I18n.t('result.length-of-stay.high_trim_point'),nil, nil],
+    [low_trim_point, base_cost_rate, 'lo', I18n.t('result.length-of-stay.low_trim_point'),
+        make_tooltip('low_trim_point', low_trim_point, base_cost_rate), nil],
+    [avg_duration, base_cost_rate, 'avg',I18n.t('result.length-of-stay.average_los'),
+        make_tooltip('average_los', avg_duration, base_cost_rate), avg_transfer_rate],
+    [high_trim_point, base_cost_rate, 'hi', I18n.t('result.length-of-stay.high_trim_point'),
+        make_tooltip('high_trim_point', high_trim_point, base_cost_rate), nil],
     [many_days, many_days_cost_rate, '','',nil, nil]
     
     sorted_rows = rows.sort_by { |row| row[0] }
@@ -55,5 +57,10 @@ class LosDataTable < GoogleVisualr::DataTable
                 :hAxis => { :title => 'Tage' },
                 :vAxis => { :title => 'Kostengewicht' }}
     GoogleVisualr::Interactive::LineChart.new(self, options)
+  end
+  
+  #TODO: values have to be bold
+  def make_tooltip(x_value_caption, x_value, y_value)
+    "#{I18n.t('result.length-of-stay.' + x_value_caption)}: #{x_value.to_s} \\n #{I18n.t('result.cost-weight.legend')}: #{y_value.to_s}"
   end
 end
