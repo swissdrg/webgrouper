eb39ff499abba1a357fd19fbfccae23559cb83eb
panmari <smmuzi@gmail.com>
2012-05-06 19:54:45 +0200

    refactored js into several files

--- app/assets/javascripts/webgrouper_patient_cases.js
+++ app/assets/javascripts/webgrouper_patient_cases.js
@@ -11,42 +11,6 @@ $("#system_SyID").live("change keyup", function () {
 	this.form.submit();
 });
 
-$(".calc_los").live("focus change", function() {
-	var first = parseDate($('#webgrouper_patient_case_entry_date').val())
-	var second = parseDate($('#webgrouper_patient_case_exit_date').val())
-	var leave_days = $('#webgrouper_patient_case_leave_days').val()
-	var diff = daydiff(first, second, leave_days);
-	if (!(isNaN(diff))){
-		$('#webgrouper_patient_case_los').val(diff);
-		flashYellow($("#webgrouper_patient_case_los"),'transparent',75,20,4 );
-	}
-});
-
-$("#webgrouper_patient_case_birth_date").live("focus change", function() {
-	if ($('#webgrouper_patient_case_birth_date').val() == "") {
-		return;
-	}
-	var bd = parseDate($('#webgrouper_patient_case_birth_date').val());
-	var today = new Date();
-	var year_diff = Math.floor(Math.ceil(today - bd) / (1000 * 60 * 60 * 24 * 365));
-	if (!(isNaN(year_diff)) && bd < today) {
-		if (year_diff >= 1) {
-			$('#webgrouper_patient_case_age_mode').val("year");
-			$('#webgrouper_patient_case_age').val(year_diff);
-		}
-		else {
-			$('#webgrouper_patient_case_age_mode').val("days");
-			$('#webgrouper_patient_case_age').val(daydiff(bd, today, 0));
-		}
-		admWeightControl(500);
-	}
-	else {
-		$('#webgrouper_patient_case_age').val("");
-	}
-	flashYellow($("#webgrouper_patient_case_age_mode"),'transparent',75,20,4 );
-	flashYellow($("#webgrouper_patient_case_age"),'transparent',75,20,4 );
-});
-
 /**
  * This method binds an alternative update method to every autocomplete field, so that
  * only the code itself is put into the field.
@@ -58,41 +22,6 @@ function initializeAutocomplete() {
   		event.target.value = data.item.label.substring(0, splitPos);
   		event.target.title = data.item.label.substring(splitPos + 1);
 	});
-}
-
-/**
- * Adds date pickers to every input field of the class "date_picker"
- */
-function initializeDatePickers() {
-	$(".date_picker").each(function() {
-		addDatePicker(this.id);
-	});
-}
-
-/**
- * Adds a date picker to the field with the given id
- * The format of the date picker is eg 02.04.2011
- * @param id the id of the field you want to add a datepicker
- */
-function addDatePicker(id) {
-	var eles = {};
-	eles[id] = "d-dt-m-dt-Y";
-	datePickerController.createDatePicker({formElements: eles});
-}
-
-
-/**
- * Lets the id "admWeight" disappear according to the
- * settings in the field age_mode
- * @param fade_time the time used to fade it in/out
- */
-function admWeightControl(fade_time) {
-	if ($('[id$="age_mode"]').val() == "days") {
-		$("#admWeight").show(fade_time);
-	}
-	else {
-		$("#admWeight").hide(fade_time);
-	}
 }
 
 
@@ -346,14 +275,6 @@ function remove_fields(kind) {
 	add_buttons(kind);
 }
 
-function parseDate(str) {
-    var mdy = str.split('.')
-    return new Date(mdy[2], mdy[1]-1, mdy[0]-1);
-}
-
-function daydiff(first, second, leave_days) {
-		return Math.floor(((second-first)/(1000*60*60*24))-leave_days)
-}
 
 /**
  * To highlight an element for a short time, this method will
