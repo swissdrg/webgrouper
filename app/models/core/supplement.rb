1e12c498cfe397e9a994a5b7c7bfe090aa1cb6cb
Michael Single <silent.simplay@gmail.com>
2012-05-07 16:04:38 +0200

    added db stuff

--- app/models/core/supplement.rb
+++ app/models/core/supplement.rb
@@ -1,4 +1,7 @@
 class Supplement < ActiveRecord::Base
+	has_many :OPS, :through => :supplement_ops, :foreign_key => "fee"
+	has_many :supplement_ops	
+
 	def self.table_name
 		'supplement'	
 	end
--- app/models/core/supplement_ops.rb
+++ app/models/core/supplement_ops.rb
@@ -1,4 +1,7 @@
 class SupplementOps < ActiveRecord::Base
+	belongs_to :OPS, :primary_key => "OpCode"
+	belongs_to :supplement, :primary_key => "fee"
+	
 	def self.table_name
 		'supplement_ops'	
 	end
