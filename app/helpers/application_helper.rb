6c88f0c37b91c72b2912527ff991ecbff384965e
Simon Kiener <jugglinghobo@gmail.com>
2012-05-09 19:04:04 +0200

    added first validation error features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
       
   def link_to_add_fields(name, kind)  
-    link_to_function(image_tag(name), "add_fields(\"#{kind}\", \"#{escape_javascript(row(kind))}\", \"\")")
+    link_to_function(image_tag(name), "add_fields(\"#{kind}\", \"#{escape_javascript(row(kind))}\", \"\")", :id => "add_#{kind}")
   end
   
   def link_to_remove_fields(name, kind)
-    link_to_function(image_tag(name), "remove_fields(\"#{kind}\")")
+    link_to_function(image_tag(name), "remove_fields(\"#{kind}\")", :id => "remove_#{kind}")
   end
     
   def row(kind)
