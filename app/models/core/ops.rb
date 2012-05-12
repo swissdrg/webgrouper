4cc25e2e8466b87e7885a312262c106294ca1881
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-05-12 17:47:09 +0200

    Minor tweaks and specs

--- app/models/core/ops.rb
+++ app/models/core/ops.rb
@@ -1,7 +1,6 @@
 class OPS < ActiveRecord::Base
 	has_many :names, :class_name => "OPSName", :foreign_key => "OnFkOpID" 
-	has_many :supplements, :through => :supplement_ops, :foreign_key => "ops"
-	has_many :supplement_ops
+
 
   default_scope lambda{where(:OpFkSyID => System.current_system.SyID)}
 	
--- spec/models/drg_spec.rb
+++ spec/models/drg_spec.rb
@@ -6,4 +6,13 @@ describe DRG do
     DRG.table_name.should == "drg"
   end
   
+  describe ".reuptake_exception_for?" do
+    
+    it "should return true if the flag of the given drg is true" do
+      dr_code = "A01A"
+      DRG.reuptake_exception_for?(dr_code).should be_true
+    end
+    
+  end
+  
 end
