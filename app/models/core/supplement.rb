11716ee25a575d0d67330904f977a000ef84e721
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2012-05-12 16:16:06 +0200

    Added basic functionality to get all supplements for entered procedures

--- app/models/core/supplement.rb
+++ app/models/core/supplement.rb
@@ -1,6 +1,8 @@
 class Supplement < ActiveRecord::Base
 	has_many :OPS, :through => :supplement_ops, :foreign_key => "fee"
 	has_many :supplement_ops	
+	
+	default_scope lambda{where(:system => System.current_system_id)}
 
 	def self.table_name
 		'supplement'	
