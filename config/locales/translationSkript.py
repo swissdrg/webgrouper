fc45f3b32dc1130757213da7c530923e67e4313f
panmari <smmuzi@gmail.com>
2012-05-16 19:35:14 +0200

    translation skript adds empty strings to missing stuff!

--- config/locales/translationSkript.py
+++ config/locales/translationSkript.py
@@ -1,27 +1,39 @@
 #! /usr/bin/env python
 import yaml
+import os
 
-de_file = open("de.yml")
-de_yml = yaml.load(de_file)
-
-languages = ["fr", "it", "en"]
-
-for lang in languages:
-	lang_yml = yaml.load(open(lang + ".yml"))
+def addMissingKeys(keys_de, keys_other):
+	if type(keys_other) is str:
+		return
+	for key, value in keys_de.items():
+		if not key in keys_other:
+			if type(value) is dict:
+				keys_other[key] = {}
+			else:
+				keys_other[key] = ' '
+		if type(value) is dict:
+			addMissingKeys(keys_de[key], keys_other[key])
+			
+def main():
+	de_file = open("de.yml")
+	de_yml = yaml.load(de_file)
+	print "loaded german yml file"
+	languages = ["fr", "it", "en"]
 	
+	for lang in languages:
+		lang_yml = yaml.load(open(lang + ".yml"))
+		addMissingKeys(de_yml["de"], lang_yml[lang])
+		test_file_path = 'test_'+ lang + '.yaml'
+		os.remove(test_file_path)
+		stream = file(test_file_path, 'w')
+		yaml.dump(lang_yml, stream,  default_flow_style=False, encoding=('utf-8'))
+	print 'Terminated!'
+
+def initSwissDRGdict():
 	swissdrg_lang = open("messages_" + lang + ".properties")
 	for line in swissdrg_lang:
 		key, value = line.split("=")
 		swissdrg_dict[key] = value
-	
-	addMissingKeys(
-
-def addMissingKeys(keys_de, keys):
-	for key, value in keys_de.items():
-		if not key in keys:
-			lang_value = blub
-			keys[key] = lang_value
-
-
-def initKeyChart(swissdrg_dict, de_yml):
-	
+		
+if __name__ == "__main__":
+    main()
