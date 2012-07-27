07611a7bff85b93dfd2fe902a4d4d6fd3bd1b58f
panmari <smmuzi@gmail.com>
2012-07-27 14:39:11 +0200

    added deployment configs for torquebox

--- config/torquebox.rb
+++ config/torquebox.rb
@@ -1,8 +1,14 @@
 TorqueBox.configure do
   pool :web, :type => :shared
   
+  web :context => "/webgrouper"
+  
   #Use 1.9 syntax
   ruby do
     version "1.9"
+  end
+  
+  environment do
+    RAILS_ENV 'production'
   end
 end
