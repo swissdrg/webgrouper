0b09d55710538ea2a983e55480307f9a87ff2819
panmari <smmuzi@gmail.com>
2012-08-30 20:03:56 +0200

    Added link to def manual

--- app/helpers/webgrouper_patient_cases_helper.rb
+++ app/helpers/webgrouper_patient_cases_helper.rb
@@ -18,4 +18,12 @@ module WebgrouperPatientCasesHelper
   def remove_button(kind)
     render "shared/remove_button", :kind => kind
   end
+  
+  # valid types are: drgs, mdcs, adrgs, tables, icd_codes, chop_codes, functions
+  # (given as strings)
+  def link_to_online_definition_manual(code, type, system_id)
+    # TODO: use system_id
+    system_url = "http://webapps.swissdrg.org:8081/"
+    return link_to code, system_url + type.to_s + "/name?code=" + code + "&?locale=" + I18n.locale.to_s
+  end
 end
