ca27ecc7c5d60eca73e19b7fe24f2978ebfaffbc
panmari <smmuzi@gmail.com>
2012-09-04 12:11:10 +0200

    Added some tiny error handling & titles

--- app/controllers/batchgroupers_controller.rb
+++ app/controllers/batchgroupers_controller.rb
@@ -2,15 +2,22 @@ class BatchgroupersController < ApplicationController
   respond_to :html, :json
   
   def index
+    @title = 'Batchgrouper'
     @batchgrouper = Batchgrouper.new()
   end
   
   def group
+    @title = 'Batchgrouper'
     @batchgrouper = Batchgrouper.new(params[:batchgrouper])
     if params[:batchgrouper][:file]
-      send_file @batchgrouper.group
+      begin
+        send_file @batchgrouper.group 
+      rescue
+        flash.now[:error] = @batchgrouper.errors.full_messages
+        render 'index'
+      end
     else
-      @single_group_result = @batchgrouper.group_line(@batchgrouper.single_group)
+      @single_group_result = @batchgrouper.group_line(@batchgrouper.single_group) rescue t('batchgrouper.invalid_format')
       render 'index'
     end
   end
