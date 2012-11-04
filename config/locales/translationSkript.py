e4232e8570c9964b10af6f8447cea91e8f59b214
panmari <smmuzi@gmail.com>
2012-11-04 15:58:45 +0100

    rewrote translation script, now filling in "null" when translation is missing

--- config/locales/translationSkript.py
+++ config/locales/translationSkript.py
@@ -11,76 +11,47 @@ import yaml
 import os
 import codecs
 
-swissdrg_dict = {}
-
-def addMissingKeys(keys_de, keys_other, lang):
+def fillInNewTranslations(keys_de, keys_other, new_translations):
 	if type(keys_other) is str:
 		return
 	for key, value in keys_de.items():
-		if not key in keys_other:
+		if not key in keys_other or needsTranslation(keys_other[key]):
 			if type(value) is dict:
 				keys_other[key] = {}
 			else:
-				swissdrg_key = get_swissdrg_key(value)
-				if swissdrg_key != None:
-					keys_other[key] = swissdrg_dict[lang][swissdrg_key]
+				if key in new_translations:
+					keys_other[key] = new_translations[key]
 				else:
-					keys_other[key] = "Translation missing"
+					keys_other[key] = None
 		if type(value) is dict:
-			addMissingKeys(keys_de[key], keys_other[key], lang)
+			fillInNewTranslations(keys_de[key], keys_other[key], new_translations)
 			
-# Returns the key if the given value has a matching swissdrg key
-# TODO: Initialize a lookup-Dictionary
-def get_swissdrg_key(search_value):
-	# try to remove a leading colon
-	if type(search_value) is str:
-		search_value_without = search_value.replace(":", "")
-		print search_value_without
-	else:
-		search_value_without = search_value
-	for key, value in swissdrg_dict["de"].items():
-		if value == search_value or value == search_value_without:
-			return key
-	return None
+def needsTranslation(value):
+	if value is None:
+		return True
+	else: 
+		return value == u""
 			
 def main():
 	languages = ["fr", "it", "en"]
 	files = ["", "simple_form."]
-	init_swissdrg_dicts()
+	
+	new_trans = dict(en="translations_webgrouper_batchgrouper-e.txt",
+					 fr="translations_webgrouper_batchgrouper-f.txt",
+					 it="translations_webgrouper_batchgrouper-i.txt")
 	
 	for f in files:
 		de_file = codecs.open("old_locales/" + f + "de.yml", "r", "utf-8")
 		de_yml = yaml.load(de_file)
 		for lang in languages:
+			new_trans_yaml = yaml.load(codecs.open("old_locales/" + new_trans[lang], "r", "ISO-8859-1"))
 			lang_yml = yaml.load(codecs.open("old_locales/" + f + lang + ".yml", "r", "utf-8"))
 			test_file_path = f + lang + '.yml'
 			print "Trying to complete " + test_file_path
-			addMissingKeys(de_yml["de"], lang_yml[lang], lang)
+			fillInNewTranslations(de_yml["de"], lang_yml[lang], new_trans_yaml)
 			stream = file(test_file_path, 'w')
 			yaml.safe_dump(lang_yml, stream, allow_unicode=True, default_flow_style=False)
 	print 'Terminated!'
-
-def init_swissdrg_dicts():
-	global swissdrg_dict
-	for lang in ["de", "en", "it", "fr"]:
-		swissdrg_lang = codecs.open("old_locales/" + "messages_" + lang + ".properties.utf", "r", "utf-8")
-		dict = {}
-		swissdrg_dict[lang] = dict
-		for line in swissdrg_lang:
-			if not "#" in line:
-				key, value = line.split("=", 1)
-				dict[key] = sanitizeString(value)
-
-# The strings are a bit random, so they need sanitizing
-def sanitizeString(string):
-	string = string.strip()
-	string = string.replace("\u00F6", "ö")
-	string = string.replace("\u00DF", "ss")
-	string = string.replace("\u00E4", "ä")
-	string = string.replace("\u00FC", "ü")
-	string = string[0:1].upper() + string[1:]
-	print string
-	return string
 	
 if __name__ == "__main__":
     main()
