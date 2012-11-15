46cfbe0bc671e71b1065b16d1c4e4894a90d0559
panmari <smmuzi@gmail.com>
2012-11-15 14:45:41 +0100

    now showing description of MDC in result

---
+++ app/models/core/mdc.rb
@@ -0,0 +1,15 @@
+class Mdc
+  include Mongoid::Document
+  field :code, type: String
+  field :text, type: String, localize: true
+  field :version, type: String
+  field :prefix, type: String
+  
+  scope :in_system, lambda { |system_id| where(:version => System.where(:system_id => system_id ).first.drg_version) }
+
+  index :text => 1, :version => 1
+  
+  def self.get_description_for(system_id, search_code)
+    in_system(system_id).where(:code => search_code).first.text
+  end
+end
