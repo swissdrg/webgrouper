2f415f026748e0519fec2070a76acd6c52e3dcae
panmari <smmuzi@gmail.com>
2012-11-22 10:14:21 +0100

    revised birthhouse DRG logic in weighting relation, so los-average, hi, lo is still set

--- app/models/webgrouper_weighting_relation.rb
+++ app/models/webgrouper_weighting_relation.rb
@@ -1,4 +1,5 @@
 # Acts as a wrapper class for superclass WeightingRelation
+# Has some special logic for System V1.0 and birthhouse DRGs
 class WebgrouperWeightingRelation < WeightingRelation
 	attr_accessor :factor	
 	def initialize(drg_code, house, system_id)
@@ -10,16 +11,21 @@ class WebgrouperWeightingRelation < WeightingRelation
 		  drg = Drg.find_by_code(system_id, drg_code)
 		end
 		
-		unless (house == "2" and drg.nil?) # a non birthhouse DRG in a birthhouse => leave everything 0
-  		self.setDrg(drg.code)
-  		self.setCostWeight(drg.cost_weight*@factor)
-  		self.setAvgDuration(drg.avg_duration*@factor)
-  		self.setFirstDayDiscount(drg.first_day_discount)
-  		self.setFirstDaySurcharge(drg.first_day_surcharge)
-  		self.setSurchargePerDay(drg.surcharge_per_day*@factor)
-  		self.setDiscountPerDay(drg.discount_per_day*@factor)
-  		self.setTransferFlatrate(drg.transfer_flatrate*@factor)
-  		self.setUseTransferFlatrate(drg.transfer_flag)
+		# a non birthhouse DRG in a birthhouse => cost-weight 0
+		# take all other information from normal DRG
+		if (house == "2" and drg.nil?) 
+  		drg = Drg.find_by_code(system_id, drg_code)
+    else
+      self.setCostWeight(drg.cost_weight*@factor)
+      self.setSurchargePerDay(drg.surcharge_per_day*@factor)
+      self.setDiscountPerDay(drg.discount_per_day*@factor)
+      self.setTransferFlatrate(drg.transfer_flatrate*@factor)
+      self.setUseTransferFlatrate(drg.transfer_flag)
     end
+    # These values needs to be set in all cases
+    self.setDrg(drg.code)
+    self.setAvgDuration(drg.avg_duration*@factor)
+    self.setFirstDayDiscount(drg.first_day_discount)
+    self.setFirstDaySurcharge(drg.first_day_surcharge)
 	end
 end
