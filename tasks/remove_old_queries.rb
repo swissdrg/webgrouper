e04e0419ea12f41593f1dcb74c6b66f615538844
panmari <smmuzi@gmail.com>
2013-03-06 10:50:23 +0100

    moved some legacy code around

--- tasks/remove_old_queries.rb
+++ tasks/remove_old_queries.rb
@@ -1,11 +1,12 @@
+# Not in use right now!
 class RemoveOldQueries
   def run
     ids = Query.where("created_at < ?", 30.days.ago)
     if ids.size > 0
       Query.destroy(ids)
       puts "#{ids.size} queries have been deleted"
-      else 
-        puts "No queries were old enough for deleting"
-      end
+    else
+      puts "No queries were old enough for deleting"
     end
+  end
 end
