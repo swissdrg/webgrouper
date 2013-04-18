47426a5eae08686145d45dd6ef8a05af8e674d84
panmari <smmuzi@gmail.com>
2013-04-18 23:41:14 +0200

    made page crawlable by googlebot (and usable by non-browsers!)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first || I18n.default_locale
+     I18n.locale = params[:locale] || request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first rescue I18n.default_locale
   end
   
   def default_url_options(options={})
