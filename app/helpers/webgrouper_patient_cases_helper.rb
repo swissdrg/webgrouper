eae1fa73f71f9e277936117431276c47329788fa
panmari <smmuzi@gmail.com>
2013-04-18 09:48:28 +0200

    fixed manual_url showing up for versions that don't have a manual

--- app/helpers/webgrouper_patient_cases_helper.rb
+++ app/helpers/webgrouper_patient_cases_helper.rb
@@ -23,10 +23,10 @@ module WebgrouperPatientCasesHelper
   # (given as strings)
   def link_to_online_definition_manual(code, type, system_id)
     manual_url = System.where(:system_id => system_id).first.manual_url
-    if manual_url
-      return link_to code, manual_url + type.to_s + "/name?code=" + code + "&locale=" + I18n.locale.to_s, :title => t('to_online_definition_manual')
+    if manual_url.blank?
+      code
     else
-      return code
+      link_to code, manual_url + type.to_s + "/name?code=" + code + "&locale=" + I18n.locale.to_s, :title => t('to_online_definition_manual')
     end
   end
 end
