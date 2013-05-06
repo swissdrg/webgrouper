dcc2705f35b81a7b6e215a1906fd0992e0a20cf8
panmari <smmuzi@gmail.com>
2013-05-06 13:54:19 +0200

    made loading of new kernel much simpler, since no other systems than linux are considered

--- config/initializers/load_grouper_kernel.rb
+++ config/initializers/load_grouper_kernel.rb
@@ -15,27 +15,11 @@ java_import Java::org.swissdrg.grouper.GrouperResult
 java_import Java::org.swissdrg.grouper.WeightingRelation
 java_import Java::org.swissdrg.grouper.EffectiveCostWeight
 
-# Load libraries according to system:
-# This is a bit complicated due to obscure naming convention.
-if java.lang.System.getProperty('os.arch').include?('64')
-  arch_bin = '64'
-  arch_lib = '64bit'
-else 
-  arch_bin = '32'
-  arch_lib = ''
-end
-
-if java.lang.System.getProperty('os.name').downcase.include?('windows')
-  lib_prequel = ''
-  file_extension = '.dll'
-else 
-  lib_prequel = 'lib'
-  file_extension = '.so.3.0.0'
-end
-
 DEFAULT_SYSTEM = 13
 
 # The real grouper:
 spec_path = spec_path(DEFAULT_SYSTEM)
-grouper_path = File.join(Rails.root, 'lib', lib_prequel + 'GrouperKernel' + arch_lib + file_extension)
+
+grouper_path = File.join(spec_folder, 'libGrouperKernel64.so.3.0.0')
+
 GROUPER = org.swissdrg.grouper.kernel.GrouperKernel.create(grouper_path, spec_path)
