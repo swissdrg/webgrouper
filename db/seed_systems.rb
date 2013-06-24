64e4dd05587cf8f0c93b6e3f3d9089cad6f87e28
panmari <smmuzi@gmail.com>
2013-06-24 14:05:29 +0200

    Added now seeding of systems to rake seeds

---
+++ db/seed_systems.rb
@@ -0,0 +1,16 @@
+require 'csv'
+
+CSV.foreach('db/systems.csv', :headers => true, :converters => :all, :col_sep => ';') do |row|
+  s = System.find_or_create_by(:system_id => row[0])
+  s.description_translations = {"de" => row[1], "fr" => row[2], "it" => row[3]}
+  s.chop_version = row[4]
+  s.icd_version = row[5]
+  s.drg_version = row[6]
+  s.manual_url = row[7]
+  if row[8].downcase == 'true'
+    s.public = true
+  else
+    s.public = false
+  end
+  s.save
+end
