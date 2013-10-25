a85bc7e00b5911639335f71291255e159de8c445
panmari <smmuzi@gmail.com>
2013-10-25 15:53:21 +0200

    more verbose response when adding system
    Returning now a json object with id of new system
    
    also checking if system already exists and reference to old system if
    does.

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -5,24 +5,30 @@ class SystemsController < ApplicationController
   # Used by the drg-dev tool to create systems for internal use only.
   # Should not be present in public version!
   def create
-    s = System.create(params[:system])
-    s.public = true
-    s.save
-    status = 500
-    if s.errors.any?
-      notice = s.errors.full_messages.join("\n")
+    s = System.new(params[:system])
+    if System.where(:description => s.description).exists?
+      render :json => {:text => 'System schon vorhanden',
+                       :system_id => System.where(:description => s.description).first.system_id }
     else
-      if params[:spec_file]
-        path = File.join(spec_folder, s.system_id.to_s, 'Spec64bit.bin')
-        FileUtils.mkdir(File.dirname(path))
-        File.open(path, 'wb') { |f| f.write(params[:spec_file].read) }
-        notice = 'System erfolgreich hinzugefuegt'
-        status = 200
+      s.public = true
+      s.save
+      status = 500
+      if s.errors.any?
+        notice = s.errors.full_messages.join("\n")
       else
-        notice = 'Kein Spezifikationsfile angehaengt, konnte nicht speichern'
-        s.destroy
+        if params[:spec_file]
+          path = File.join(spec_folder, s.system_id.to_s, 'Spec64bit.bin')
+          FileUtils.mkdir(File.dirname(path))
+          File.open(path, 'wb') { |f| f.write(params[:spec_file].read) }
+          notice = 'System erfolgreich hinzugefuegt'
+          status = 200
+        else
+          notice = 'Kein Spezifikationsfile angehaengt, konnte nicht speichern'
+          s.destroy
+        end
       end
+      render :json => {:text => notice, :system_id => s.system_id}, :status => status
     end
-    render :text => notice, :status => status
   end
+
 end
