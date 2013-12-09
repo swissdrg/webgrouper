5636ad30e4a8bb7a8e92c43366dddf861f623541
panmari <smmuzi@gmail.com>
2013-12-09 23:54:43 +0100

    added comments and some small fixes for DRG seeding

--- db/seed_drgs.rb
+++ db/seed_drgs.rb
@@ -1,10 +1,10 @@
 require 'db/seed_helpers'
 include SeedHelpers
 Drg.delete_all
-conn.exec("SELECT * FROM classifications.drgs" ).each do |row|
+conn.exec("SELECT * FROM classifications.drgs").each do |row|
   # rename some attributes
   row['avg_duration'] = row.delete 'average_stay_duration'
-  row['transfer_discount'] = row.delete 'transfer_flatrate'
+  row['transfer_flatrate'] = row.delete 'transfer_discount'
   row['exception_from_reuptake_flag'] = row.delete 'exception_from_reuptake'
   save_code(Drg, row)
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
-
+# Sorry, this is dead slow, but automatically does all stuff necessary like creating indizes etc, so nothing can
+# get forgotten. If you just want to seed a single model, execute the appropriate command in the rails console.
+# For seeding to the production database, you'll need to open an ssh tunnel, since mongodb is not accessible from
+# the outside, eg with:
+# ssh sm@77.95.120.68 -L 27017:77.95.120.68:27017 -N
+start = Time.now
 load 'db/seed_systems.rb'
 load 'db/seed_icds.rb'
 load 'db/seed_chops.rb'
@@ -13,3 +11,4 @@ load 'db/seed_drgs.rb'
 load 'db/seed_supplements.rb'
 load 'db/seed_supplement_descriptions.rb'
 load 'db/seed_mdcs.rb'
+puts "Took #{(Time.now - start).to_s} seconds"
