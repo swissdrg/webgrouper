f51a160123fe1278648fadce709470917adff5bb
panmari <smmuzi@gmail.com>
2014-03-19 14:01:14 +0100

    added config to remove deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,9 @@ module Webgrouper
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    # See http://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning
+    config.i18n.enforce_available_locales = false # we want to redirect to default locale
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
