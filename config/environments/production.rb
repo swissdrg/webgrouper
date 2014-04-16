1e81e61cce07a4d6b467fb9ecc9e884024fbb8cd
panmari <smmuzi@gmail.com>
2014-04-16 22:14:22 +0200

    added batchgrouper.js to precompiled files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Webgrouper::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( webgrouper.js )
+  config.assets.precompile += %w( webgrouper.js batchgrouper.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
