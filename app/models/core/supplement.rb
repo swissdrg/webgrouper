6f877793bf116798e1120ff66832c196b3b63d6e
panmari <smmuzi@gmail.com>
2014-05-14 15:14:43 +0200

    added some unimportant fields to supplement
    that may or may not be used later on

--- app/models/core/supplement.rb
+++ app/models/core/supplement.rb
@@ -4,10 +4,17 @@ class Supplement
   field :supplement_code, type: String
   field :amount, type: Float
   field :version, type: String
-  field :route_of_administration, type: String
   field :text, type: String, localize: true
 
-  scope :in_system, lambda { |system_id| where(:version => System.where(:system_id => system_id ).first.drg_version) }
+  # These are not used actually, but available in the database for V4.0 and up
+  field :route_of_administration, type: String
+  field :age_max, type: Integer
+
+  field :dose_min, type: Float
+  field :dose_max, type: Float
+  field :dose_unit, type: String
+
+  scope :in_system, lambda { |system_id| where(:version => System.find_by(:system_id => system_id ).drg_version) }
 
   index :chop_code => 1, :version => 1
 end
