7b5ecd816c592c4a67a167f0916612c6e89d8c30
Urs Gerber <ug.gerber@gmail.com>
2014-07-23 09:55:18 +0200

    Add new system, fix system order bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,4 +41,13 @@ module ApplicationHelper
   def batchgroupings_temp_folder
     File.join("/tmp", "batchgroupings")
   end
+
+  def shown_systems
+    if session[:beta]
+      shown_systems = System.all
+    else
+      shown_systems = System.all_public
+    end
+    shown_systems.order_by(:id.desc)
+  end
 end
