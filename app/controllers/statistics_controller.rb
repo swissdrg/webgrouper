2415ac22b458517225ff7a08dacbea4152839c19
panmari <smmuzi@gmail.com>
2015-01-21 15:20:07 +0100

    Added size chart for webapi queries.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -11,14 +11,16 @@ class StatisticsController < ApplicationController
     @bg_size_chart = make_size_chart(@bg_queries, :line_count, bins)
   end
 
+  def webapi
+    @wa_queries = WebapiQuery.where(:start_time.gt => @from, :start_time.lt => @to)
+    bins = 100 if params[:binned]
+    @wa_size_chart = make_size_chart(@wa_queries, :nr_cases, bins)
+  end
+
   def webgrouper
     @queries = Query.where(:time.gt => @from, :time.lt => @to)
     @system_chart = make_system_chart(@queries)
     @house_chart = make_house_chart(@queries)
-  end
-
-  def webapi
-    @wa_queries = WebapiQuery.where(:start_time.gt => @from, :start_time.lt => @to)
   end
 
   private
@@ -31,23 +33,23 @@ class StatisticsController < ApplicationController
 
   def make_system_chart(queries)
     system_data = GoogleVisualr::DataTable.new
-    system_data.new_column('string', 'System' )
+    system_data.new_column('string', 'System')
     system_data.new_column('number', 'Groupings')
     rows = []
     System.each do |s|
       rows << [s.description, queries.where(system_id: s.system_id).count]
     end
     system_data.add_rows(rows)
-    options = {title: 'Used systems' }
+    options = {title: 'Used systems'}
     GoogleVisualr::Interactive::PieChart.new(system_data, options)
   end
 
   def make_house_chart(queries)
     house_data = GoogleVisualr::DataTable.new
-    house_data.new_column('string', 'House' )
+    house_data.new_column('string', 'House')
     house_data.new_column('number', 'Groupings')
-    house_data.add_rows([ ['Hospital', queries.where(house: '1').count],
-                          ['Birthhouse', queries.where(house: '2').count]])
+    house_data.add_rows([['Hospital', queries.where(house: '1').count],
+                         ['Birthhouse', queries.where(house: '2').count]])
     options = {title: 'Hospital vs Birthhouse'}
     GoogleVisualr::Interactive::PieChart.new(house_data, options)
   end
@@ -59,7 +61,7 @@ class StatisticsController < ApplicationController
              data_table.new_column('number', 'Patient cases')
              queries.distinct(attribute).sort.map do |attr_count|
                [attr_count, queries.where(attribute => attr_count).count]
-              end
+             end
            else
              data_table.new_column('string', 'Patient cases')
              max_line_count = queries.max(attribute)
@@ -75,8 +77,8 @@ class StatisticsController < ApplicationController
            end
     data_table.new_column('number', 'Number of queries')
     data_table.add_rows(rows)
-    opts   = { title: 'Size', hAxis: {title: 'Number of patient cases in query'},
-               vAxis: {title: 'Number of queries', logScale: true}, legend: {position: 'none' } }
+    opts = {title: 'Size', hAxis: {title: 'Number of patient cases in query'},
+            vAxis: {title: 'Number of queries', logScale: true}, legend: {position: 'none'}}
     GoogleVisualr::Interactive::ColumnChart.new(data_table, opts)
   end
 end
