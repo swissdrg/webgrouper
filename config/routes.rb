50c4ad910b1b7f6acd55c735935ac6316f285a49
panmari <smmuzi@gmail.com>
2015-01-22 16:49:21 +0100

    Rewrote routes for rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -11,23 +11,22 @@ Webgrouper::Application.routes.draw do
       get :autocomplete_Icd_code, :on => :collection
       get :autocomplete_Chop_code, :on => :collection
     end
-    post 'create_query' => 'webgrouper_patient_cases#create_query'
-    get 'create_query' => 'webgrouper_patient_cases#create_query'
+    match 'create_query' => 'webgrouper_patient_cases#create_query', via: [:get, :post]
     get 'help' => 'static_pages#help'
     get 'tos' => 'static_pages#tos'
     post 'batchgrouper' => 'batchgroupers#group'
     get 'batchgrouper' => 'batchgroupers#index'
     get 'index' => 'webgrouper_patient_cases#index'
-    match 'parse' => 'webgrouper_patient_cases#parse'
+    match 'parse' => 'webgrouper_patient_cases#parse', via: [:get, :post]
   end
 
 
   scope 'webapi' do
     get '/' => 'webapi#index'
-    match 'grouper/group' => 'webapi#group'
-    match 'grouper/systems' => 'webapi#systems'
-    match 'group' => 'webapi#group', :as => :webapi_group
-    match 'systems' => 'webapi#systems', :as => :webapi_systems
+    match 'grouper/group' => redirect('/webapi/group'), via: [:get, :post]
+    match 'grouper/systems' => redirect('/webapi/systems'), via: [:get, :post]
+    match 'group' => 'webapi#group', :as => :webapi_group, via: [:get, :post]
+    match 'systems' => 'webapi#systems', :as => :webapi_systems, via: [:get, :post]
   end
 
   get 'grouper' => 'batchgroupers#tos'
