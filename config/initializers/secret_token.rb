dc102934a0adff899f936f7c35a471c441e1e759
panmari <smmuzi@gmail.com>
2015-01-22 17:08:15 +0100

    Ran rake rails:update

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Webgrouper::Application.config.secret_token = '6b5d69ebf77f5a5c9235a7533aaa8cf89465c6030a79dc146c59828e96afe3095aeb8d24d4336dc32dd8671d05c7f1435730ff6b9d908031a9b3ee155a660b8a'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Configure the TorqueBox Servlet-based session store.
 # Provides for server-based, in-memory, cluster-compatible sessions
 if ENV['TORQUEBOX_APP_NAME']
-  Webgrouper::Application.config.session_store TorqueBox::Session::ServletStore
+  Rails.application.config.session_store TorqueBox::Session::ServletStore
 else
-  Webgrouper::Application.config.session_store :cookie_store, :key => '_CHANGEME_session'
-end  
+  Rails.application.config.session_store :cookie_store, key: '_webgrouper_session'
+end
