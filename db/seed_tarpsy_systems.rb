8bf6ece13692d76e9b0543e1af71074b60b89f14
panmari <smmuzi@gmail.com>
2015-06-08 14:34:29 +0200

    Seeding tarpsy systems with correct ICD catalogues.

--- db/seed_tarpsy_systems.rb
+++ db/seed_tarpsy_systems.rb
@@ -7,7 +7,7 @@ LumpSumType.values.each_with_index do |lst, idx|
   TarpsySystem.create!({
     system_id: idx + 1,
     description: 'Testsystem lump sum type ' + lst.to_s,
-    icd_version: 'ICD10-GM-2014',
+    icd_version: 'ICD10-GM-2012',
     lump_sum_type: lst.to_s,
     public: true
   })
