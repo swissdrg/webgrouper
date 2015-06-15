6d2d1c4ed43aad131405e4f57bba85f67b574287
panmari <smmuzi@gmail.com>
2015-06-15 14:06:13 +0200

    Added error to main record in addition to record.

--- app/models/validators/assessments_validator.rb
+++ app/models/validators/assessments_validator.rb
@@ -4,10 +4,12 @@ class AssessmentsValidator < ActiveModel::EachValidator
     first_assessment = value.first
     if first_assessment.date < record.entry_date
       first_assessment.errors.add(:date, make_error_message('on_or_after', record.entry_date))
+      record.errors.add(:assessments, 'First assessment has invalid date')
     end
     last_possible_date_for_first_assessment = record.entry_date + 3.days
     if first_assessment.date > last_possible_date_for_first_assessment
       first_assessment.errors.add(:date, make_error_message('on_or_before', last_possible_date_for_first_assessment))
+      record.errors.add(:assessments, 'First assessment has invalid date')
     end
     # TODO: Make sure that assessment are not further than 14 days apart.
   end
