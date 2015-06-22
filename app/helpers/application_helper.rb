372f664be9907340829364d7f045d05370edde02
panmari <smmuzi@gmail.com>
2015-06-22 17:04:08 +0200

    Fixed changing language for generate_random link.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,11 +15,11 @@ module ApplicationHelper
   # If there is a form on the current page, we must submit it to make sure no content in the form is lost
   # instead of doing a simple get on the current page.
   def link_to_language_change(label, locale)
-    url = url_for(:locale => locale)
     if @webgrouper_patient_case || @tarpsy_patient_case
+      url = url_for(:locale => locale, action: 'create')
       link_to label, '#', class: 'button', onclick: "submitFormTo('#{url}');"
     else
-      link_to label, url, class: 'button'
+      link_to label, url_for(:locale => locale), class: 'button'
     end
   end
 end
