ce2cb278a41b47928c6b322a4796a3fd9261d150
panmari <smmuzi@gmail.com>
2015-06-22 16:22:02 +0200

    I18n more strings.

--- db/seed_tarpsy_systems.rb
+++ db/seed_tarpsy_systems.rb
@@ -4,9 +4,10 @@ java_import org.swissdrg.grouper.tarpsy.ITARPSYGrouperKernel::PerCasePaymentType
 
 
 PerCasePaymentType.values.each_with_index do |lst, idx|
+  lst_number = lst.to_s.scan(/\d+/).first
   TarpsySystem.create!({
                            system_id: idx + 1,
-                           description: 'Testsystem per case payment type ' + lst.to_s,
+                           description: "Tarpsy 0.2 Fallpauschale #{lst_number}%",
                            icd_version: 'ICD10-GM-2012',
                            per_case_payment_type: lst.to_s,
                            public: true
