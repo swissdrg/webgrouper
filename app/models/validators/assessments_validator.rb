deecb2db3e4b264496226aeb0a1898761f5fc262
panmari <smmuzi@gmail.com>
2015-06-30 17:25:04 +0200

    Refined assessment validator.
    Does now work correctly if date of first assessment was nil. Thanks
    @lunas for finding the bug.

--- app/models/validators/assessments_validator.rb
+++ app/models/validators/assessments_validator.rb
@@ -2,14 +2,15 @@
 class AssessmentsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     first_assessment = value.first
-    if first_assessment.date < record.entry_date
+    if first_assessment.date.nil? or first_assessment.date < record.entry_date
       first_assessment.errors.add(:date, make_error_message('on_or_after', record.entry_date))
-      record.errors.add(:assessments, 'First assessment has invalid date')
+      record.errors.add(:assessments) unless record.errors.added? :assessments
+      return
     end
     last_possible_date_for_first_assessment = record.entry_date + 3.days
     if first_assessment.date > last_possible_date_for_first_assessment
       first_assessment.errors.add(:date, make_error_message('on_or_before', last_possible_date_for_first_assessment))
-      record.errors.add(:assessments, 'First assessment has invalid date')
+      record.errors.add(:assessments) unless record.errors.added? :assessments
     end
     # TODO: Make sure that assessment are not further than 14 days apart.
   end
